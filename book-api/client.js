120c063fa8db383bf9b030df0147e5cfcf8f88e1
CinthiaRod <Cinthiarb28@gmail.com>
2025-03-11 19:56:30 -0700

    Modificación cliente

--- book-api/client.js
+++ book-api/client.js
@@ -1,51 +1,9 @@
 //Importar modulos
+const { log } = require('console');
 const net =require('net');
-const readlineSync =require('readline-sync');
+//const readlineSync =require('readline-sync');
+//NOTA AGREGAR-IMPORTAR CONTROLLERS
 
-//Comunicacion TCP
-const client = net.createConnection({ port: 8080, host: 'localhost' });
-
-
-// Maneja el evento 'connect' cuando la conexión se haya establecido.
-client.on('connect', () => {
-    // Muestra en consola que el cliente está conectado al servidor.
-    console.log('Conectado al servidor');
-    // Llama a la función para enviar el primer mensaje.
-    sendMessage();
-});
-
-// Maneja el evento 'data' para recibir mensajes del servidor.
-client.on('data', (data) => {
-    // Muestra en consola la respuesta recibida del servidor.
-    console.log('Respuesta del servidor: ' + data.toString());
-    // Vuelve a llamar a la función para enviar el siguiente mensaje.
-    sendMessage();
-});
-
-// Maneja el evento 'error' para mostrar cualquier error que ocurra.
-client.on('error', (err) => {
-    console.log('Error:', err.message);
-});
-
-
-///////////////////////////////////////////////////
-//PENDIENTE
-// Función para enviar mensajes al servidor.
-function sendMessage() {
-    // Solicita al usuario ingresar un mensaje para enviar al servidor.
-    const message = readline.question('Ingrese un mensaje para enviar al servidor: ');
-    // Si el mensaje es 'exit', cierra la conexión.
-    if (message.toLowerCase() === 'exit') {
-        client.end();
-    } else {
-        // Envía el mensaje al servidor.
-        client.write(message);
-    }
-}
-
-
-/////////////////////////////////////////////////////////
-//Ejemplo Bernie (Solucion al simulacro de examen)
 const readline = require('readline');
 
 const rl = readline.createInterface({
@@ -53,27 +11,88 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-const clientt = new net.Socket();
+const client = new net.Socket();
 
-clientt.connect(8080, 'localhost', ()=>{
+client.connect(8080, 'localhost', ()=>{
     console.log('Conectado al servidor');
+    console.log(`\nMenú de comandos:\n`);
+    console.log("1. Add");
+    console.log("2. Delete");
+    console.log("3. Modify");
+    console.log("4. List");
+    console.log("5. Exit");
     
-    rl.question('Escribe un comando (Por ejemplo: GET BOOK o ADD BOOK {"name": "El Principito", "author": "Antoine de Saint-Exupery", "publisher":"La travesia", "anio": 2022)}: ', (command) => {
-        clientt.write(command);
+    
+    rl.question('Escribe un comando:', (command) => {
+        client.write(command);
+        switch (command){
+            case "1":
+                add();
+                rl.question('Deseas agregar un libro, un autor o una editorial: ', (command) => {
+                    client.write(command);
+                    switch (command){
+                        case "autor":
+                            authorController.addAuthor();
+                            break;
+                        case "libro":
+                            bookController.addBook();
+                            break;
+                        case "editorial":
+                            publishersController.addPublisher();
+                            break;
+                        default:
+                            console.log('Comando invalido');    
+                            break;
+                    
+            case "2":
+                eliminate();
+                break;
+            case "3":
+                modify( );
+                break;
+            case "4":
+                list();
+                break;
+            case "5":
+                console.log("Exiting...");
+                break;
+            default:
+                console.log("Opcion invalida.");
+        }
     })
 })
 
 
-clientt.on('data', (data) => {
-    console.log('Respuesta del servidor: ');
-    console.log(data.toString());
+//client.on('data', (data) => {
+  //  console.log('Respuesta del servidor: ');
+    //console.log(data.toString());
 
-    rl.close();
+    //rl.close();
 
-    clientt.destroy();
-});
+   // client.destroy();
+//});
 
 
-clientt.on('close', ()=>{
-    console.log('Conexion cerrada');
-})
+//client.on('close', ()=>{
+  //  console.log('Conexion cerrada');
+//})
+
+
+///////////////////////////////////////////////////
+//PENDIENTE
+// Función para enviar mensajes al servidor.
+//function sendMessage() {
+    // Solicita al usuario ingresar un mensaje para enviar al servidor.
+  //  const message = readline.question('Ingrese un mensaje para enviar al servidor: ');
+    // Si el mensaje es 'exit', cierra la conexión.
+    //if (message.toLowerCase() === 'exit') {
+     //   client.end();
+    //} else {
+        // Envía el mensaje al servidor.
+      //  client.write(message);
+    //}
+//}
+
+
+/////////////////////////////////////////////////////////
+//Ejemplo Bernie (Solucion al simulacro de examen)
