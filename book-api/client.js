4d49d2f0dc06616c0d5275e5b1a0f748018d9901
CinthiaRod <Cinthiarb28@gmail.com>
2025-03-11 20:16:02 -0700

    Modificación cliente

--- book-api/client.js
+++ book-api/client.js
@@ -13,7 +13,7 @@ const rl = readline.createInterface({
 
 const client = new net.Socket();
 
-client.connect(8080, 'localhost', ()=>{
+client.connect(8080, 'localhost', () => {
     console.log('Conectado al servidor');
     console.log(`\nMenú de comandos:\n`);
     console.log("1. Add");
@@ -22,15 +22,14 @@ client.connect(8080, 'localhost', ()=>{
     console.log("4. List");
     console.log("5. Exit");
     
-    
-    rl.question('Escribe un comando:', (command) => {
+    rl.question('Escribe un comando: ', (command) => {
         client.write(command);
-        switch (command){
+        switch (command) {
             case "1":
                 add();
-                rl.question('Deseas agregar un libro, un autor o una editorial: ', (command) => {
-                    client.write(command);
-                    switch (command){
+                rl.question('Deseas agregar un libro, un autor o una editorial: ', (subcommand) => {
+                    client.write(subcommand);
+                    switch (subcommand) {
                         case "autor":
                             authorController.addAuthor();
                             break;
@@ -41,26 +40,81 @@ client.connect(8080, 'localhost', ()=>{
                             publishersController.addPublisher();
                             break;
                         default:
-                            console.log('Comando invalido');    
+                            console.log('Comando inválido');
                             break;
-                    
+                    }
+                });
+                break;
+                
             case "2":
                 eliminate();
+                rl.question('Deseas eliminar un libro, un autor o una editorial: ', (subcommand) => {
+                    client.write(subcommand);
+                    switch (subcommand) {
+                        case "autor":
+                            authorController.deleteAuthor();
+                            break;
+                        case "libro":
+                            bookController.deleteBook();
+                            break;
+                        case "editorial":
+                            publishersController.deletePublisher();
+                            break;
+                        default:
+                            console.log('Comando inválido');
+                            break;
+                    }
+                });
                 break;
             case "3":
-                modify( );
+                modify();
+                rl.question('Deseas modificar un libro, un autor o una editorial: ', (subcommand) => {
+                    client.write(subcommand);
+                    switch (subcommand) {
+                        case "autor":
+                            authorController.editAuthor();
+                            break;
+                        case "libro":
+                            bookController.editBook();
+                            break;
+                        case "editorial":
+                            publishersController.editPublisher();
+                            break;
+                        default:
+                            console.log('Comando inválido');
+                            break;
+                    }
+                });
                 break;
             case "4":
                 list();
+                rl.question('Deseas imprimir la lista de libros, un autores o una editoriales: ', (subcommand) => {
+                    client.write(subcommand);
+                    switch (subcommand) {
+                        case "autores":
+                            authorController.showAuthor();
+                            break;
+                        case "libros":
+                            bookController.showBook();
+                            break;
+                        case "editoriales":
+                            publishersController.showPublisher();
+                            break;
+                        default:
+                            console.log('Comando inválido');
+                            break;
+                    }
+                });
                 break;
             case "5":
                 console.log("Exiting...");
+                process.exit();  // Para salir correctamente
                 break;
             default:
-                console.log("Opcion invalida.");
+                console.log("Opción inválida.");
         }
-    })
-})
+    });
+});
 
 
 //client.on('data', (data) => {
