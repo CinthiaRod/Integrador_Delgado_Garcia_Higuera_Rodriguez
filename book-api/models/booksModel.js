f36ead741a172134b0c600153e2b30237487be2c
Paola Delgado <delgadog.paola@gmail.com>
2025-03-17 15:25:58 -0700

    Update booksModel.js

--- book-api/models/booksModel.js
+++ book-api/models/booksModel.js
@@ -1,25 +1,42 @@
+//Paso 1: Importamos modulos para el manejo de archivos
 const fs = require('fs');
 const path = require('path'); 
 
+//Paso 2: Importamos archivo que contiene la base de datos
 const dataPath = path.join(__dirname, '../data/books.json'); 
 
+//Paso 3: Generamos modulo del modelo para manipular la base de datos
 const booksModel = { 
+    //Paso 4: Generamos funcion para leer libros
     readBooks: () => { 
         try { 
-            const data = fs.readFileSync(dataPath);  
+            //Leemos la base de datos con modulo fs
+            const data = fs.readFileSync(dataPath);
+            //Desplegamos la base de datos convirtiendo de JSON a JS para su correcto
+            //manejo en el proyecto  
             return JSON.parse(data);
+        //Manejamos el error
         } catch (error) {
+            //Mostrar el error
             console.error('Error al leer el archivo books.json:', error); 
             return; 
         }
     },
 
+    //Paso 5: Generamos funcion para guardar en la base de datos
     saveBooks: (books) => { 
         try { 
+            //Escribimos en la base de datos utilizando el modulo fs
+            //convirtiendo los datos de JS a JSON
             fs.writeFileSync(dataPath, JSON.stringify(books, null, 2)); 
+        //Manejo del error
         } catch (error) {
+            //Mostrar el error
             console.error('Error al escribir en el archivo books.json:', error); 
         }
     }
 }
+
+//Paso 6: Exportamos el modulo correspondiente al modelo del libro para su uso en
+//otros archivos
 module.exports = booksModel;
